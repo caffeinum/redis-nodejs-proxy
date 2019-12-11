cb92b78c8f62326f6790125c0bdb3c5784608482
Aleksey Bykhun <theisease@gmail.com>
2019-12-11 04:21:30 +0300

    cache: basic tests for LRU and expiry

--- src/cache.spec.ts
+++ src/cache.spec.ts
@@ -1,15 +1,58 @@
 import 'mocha';
 import { expect } from 'chai';
-import storage, { writeCache, readCache } from './cache';
+import { createCacheStorage, writeCache, readCache } from './cache';
+
+const storage = createCacheStorage({
+  CACHE_CAPACITY: 3,
+  CACHE_EXPIRY_TIME: 300,
+});
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 describe('Cache module', () => {
-  it('works',
-    () => {
-      writeCache(storage, 'test1', 'Hello!');
+  it('works', () => {
+    writeCache(storage, 'test', 'Hello!');
 
-      const { error, value } = readCache(storage, 'test1');
+    const { error, value } = readCache(storage, 'test');
 
-      expect(error).to.equal(undefined);
-      expect(value).to.equal('Hello!');
-    });
+    expect(error).to.equal(undefined);
+    expect(value).to.equal('Hello!');
+  });
+
+  it('clears least recently used', () => {
+    writeCache(storage, 'test1', 'Hello One!');
+    writeCache(storage, 'test2', 'Hello Two!');
+    writeCache(storage, 'test3', 'Hello Three!');
+    writeCache(storage, 'test4', 'Hello Four!');
+    writeCache(storage, 'test5', 'Hello Five!');
+
+    const { error: error1, value: test1_value } = readCache(storage, 'test1');
+    const { error: error2, value: test2_value } = readCache(storage, 'test2');
+    const { error: error3, value: test3_value } = readCache(storage, 'test3');
+    const { error: error4, value: test4_value } = readCache(storage, 'test4');
+    const { error: error5, value: test5_value } = readCache(storage, 'test5');
+
+    expect(test1_value).to.equal(undefined);
+    expect(test2_value).to.equal(undefined);
+    expect(test3_value).to.not.equal(undefined);
+    expect(test4_value).to.not.equal(undefined);
+    expect(test5_value).to.not.equal(undefined);
+
+    expect(error1).to.equal('CACHE_MISS');
+    expect(error2).to.equal('CACHE_MISS');
+    expect(error3).to.equal(undefined);
+    expect(error4).to.equal(undefined);
+    expect(error5).to.equal(undefined);
+  });
+
+  it('cache expires', async () => {
+    writeCache(storage, 'forgotten-key', 'Should expire');
+
+    await sleep(300);
+
+    const { error, value } = readCache(storage, 'forgotten-key');
+
+    expect(error).to.equal('CACHE_EXPIRED');
+    expect(value).to.equal(undefined);
+  })
 });
--- src/cache.ts
+++ src/cache.ts
@@ -5,6 +5,12 @@ const CACHE_CAPACITY = Number(process.env.CACHE_CAPACITY || 1e5);
 // env variable in seconds, local in milliseconds
 const CACHE_EXPIRY_TIME = Number(process.env.CACHE_EXPIRY_TIME || 3600) * 1000;
 
+const unique_ordered_key = (() => {
+  let id = Date.now()
+  // let id = 0
+  return () => id++
+})();
+
 const DEFAULT_CONFIG: CacheConfig = {
   CACHE_EXPIRY_TIME,
   CACHE_CAPACITY,
@@ -15,10 +21,14 @@ interface CacheConfig {
   CACHE_EXPIRY_TIME: number,
 }
 
-// 'test_key': { value: 'test', ts: Date.now() },
+// 'test_key': { value: 'test', ts: Date.now(), pk: 1 },
 interface CacheEntry {
-  value: any;
+  // last access timestamp
   ts: number;
+  // ordered access key
+  pk: number;
+
+  value: any;
 }
 
 interface Cache {
@@ -41,7 +51,7 @@ export const createCacheStorage: (config?: any) => CacheStorage = (config = {}) 
 export const globalStorage = createCacheStorage();
 
 export const clearLRU = (storage: CacheStorage, shouldFreeSlots: number = 0) => {
-  const { cache } = storage;
+  const { cache, config: { CACHE_CAPACITY }} = storage;
 
   if (Object.keys(cache).length < CACHE_CAPACITY) {
     return;
@@ -58,24 +68,33 @@ export const clearLRU = (storage: CacheStorage, shouldFreeSlots: number = 0) => 
   //
 
   Object.keys(cache)
-    .sort((key1, key2) => cache[key1].ts - cache[key2].ts)
+    .sort((key1, key2) => cache[key2].pk - cache[key1].pk)
+    // .map(key => {
+    //   console.log('have key', key, cache[key].ts);
+    //   return key
+    // })
     .filter((_key, index) => index > lastIndex)
-    .map((key) => delete cache[key]);
+    // .map(key => {
+    //   console.log('will delete', key);
+    //
+    //   return key
+    // })
+    .map((key) => (delete cache[key]));
 };
 
 export const clearExpired = (storage: CacheStorage) => {
-  const { cache } = storage;
+  const { cache, config: { CACHE_EXPIRY_TIME } } = storage;
   // TODO: too slow, O(n)
   // a bit faster if we have had a sorted list
   // in which we would search for the first NOT expired
   // and then delete all after him
   //
   // Object.keys(cache)
-  //  .sort((key1, key2) => cache[key1].ts - cache[key2].ts)
+  //  .sort((key1, key2) => cache[key2].ts - cache[key1].ts)
 
   Object.keys(cache)
     .filter((key) => cache[key].ts + CACHE_EXPIRY_TIME < Date.now())
-    .map((key) => delete cache[key]);
+    .map((key) => (delete cache[key]));
 };
 
 export const writeCache = (storage: CacheStorage, key: string, value: any) => {
@@ -83,14 +102,16 @@ export const writeCache = (storage: CacheStorage, key: string, value: any) => {
 
   clearLRU(storage, 1);
 
-  cache[key] = { ts: Date.now(), value };
+  // console.log('write', key)
+
+  cache[key] = { ts: Date.now(), pk: unique_ordered_key(), value };
 
   // we can also check that AFTER writing
   // clearLRU(0);
 };
 
 export const readCache = (storage: CacheStorage, key: string) => {
-  const { cache } = storage;
+  const { cache, config: { CACHE_EXPIRY_TIME } } = storage;
 
   if (!cache[key]) {
     return { error: 'CACHE_MISS' };
